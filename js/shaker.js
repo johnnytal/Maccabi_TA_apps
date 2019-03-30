8248dc766144b9958f16ad5da63f36ed2f7709f8
johnnytal <johnnytal9@gmail.com>
2019-03-30 23:45:30 +0200

    simple!

--- js/shaker.js
+++ js/shaker.js
@@ -1,15 +1,14 @@
 var shakerMain = function(game){
 	FRONT_COLOR = '#c1ad65';
 	BACK_COLOR = '#656d7c';
-	
-	MIDDLE = null;
+
 	resetTouching = true;;
 
 	frontAngle = 0;
 	backAngle = 0;
 
-	INIT_FRONT = 20; // 0.82
-	INIT_BACK = -5 ; // 7.55
+	INIT_FRONT = 22;
+	INIT_BACK = -3;
 };
 
 shakerMain.prototype = {
@@ -21,46 +20,37 @@ shakerMain.prototype = {
 
     	bg = game.add.image(0, 0, 'bg');
     	bg.alpha = 0.6;
-    	
-        circles = game.add.group();
-		circles.enableBody = true;
-		circles.physicsBodyType = Phaser.Physics.ARCADE;
-		
-		circle = circles.create(0, 0, 'green');
-		circle.scale.set(0.83, 0.83);
 
+		circle = game.add.image(0, 0, 'green');
         circle.x = WIDTH / 2 - circle.width / 2;
-        MIDDLE = HEIGHT / 2 - circle.height / 2;
-        circle.y = MIDDLE;
-
-        circle.body.collideWorldBounds = true;
-        
+        circle.y = HEIGHT / 2 - circle.height / 2;
+ 
 		XtraUIbuttons();
 		
-	    debugText = game.add.text(100, 30, "" , {font: '22px', fill: 'white'});
+	    debugText = game.add.text(100, 30, "" , {font: '23px', fill: 'white'});
 		
 		try{window.addEventListener('deviceorientation', readAccel);}catch(e){}
-       // try{window.addEventListener("devicemotion",readAccel, true);} catch(e){}
     }
 };
 
 function readAccel(event){	
-
-	//circle.y = MIDDLE + event.accelerationIncludingGravity.x * (INIT_SENS + sensFactor) + distanceFactor; // (-1 * event.rotationRate.gamma)
-
-	debugText.text = 'Gamma: ' + roundIt(event.gamma);
+	angle = roundIt(event.gamma);
 	
-	if (!resetTouching && event.gamma > ((INIT_BACK + backAngle) + 3) && event.gamma < ((INIT_FRONT + frontAngle) - 3)){
-		resetTouching = true;
+	debugText.text = 'Angle: ' + angle;
+	
+	if (!resetTouching && angle > ((INIT_BACK + backAngle) + 1) && angle < ((INIT_FRONT + frontAngle) - 1)){
+		if (!front.isPlaying && !back.isPlaying){
+			resetTouching = true;
+		}
 	}
 	
 	if (game.state.getCurrentState().key == 'Shaker'){
 		if (resetTouching){
-			if (event.gamma > (INIT_FRONT + frontAngle) && !front.isPlaying && !back.isPlaying){
+			if (angle > (INIT_FRONT + frontAngle)){
 				front.play();
 				flash(FRONT_COLOR);	
 			}
-			else if (event.gamma < (INIT_BACK + backAngle) && !front.isPlaying && !back.isPlaying){
+			else if (angle < (INIT_BACK + backAngle)){
 				back.play();
 				flash(BACK_COLOR);
 			}
@@ -70,6 +60,7 @@ function readAccel(event){
 
 function flash(_color){
 	resetTouching = false;
+	
 	game.stage.backgroundColor = _color;
 	circle.tint = 0xff00df;
 	
@@ -98,7 +89,7 @@ function XtraUIbuttons(){
     plus.inputEnabled = true;
     plus.events.onInputDown.add(function(){
     	frontAngle += 0.1;
-    	frontText.text = "Front\nAngle: " + roundIt(INIT_FRONT + frontAngle);
+    	frontText.text = "FRONT: " + roundIt(INIT_FRONT + frontAngle);
     	plus.tint = 0xf04030;
     	setTimeout(function(){plus.tint = 0xffffff;},100);
     }, this);
@@ -109,7 +100,7 @@ function XtraUIbuttons(){
     minus.inputEnabled = true;
     minus.events.onInputDown.add(function(){
     	frontAngle -= 0.1;
-    	frontText.text = "Front\nAngle: " + roundIt(INIT_FRONT + frontAngle);
+    	frontText.text = "FRONT: " + roundIt(INIT_FRONT + frontAngle);
     	minus.tint = 0xf04030;
     	setTimeout(function(){minus.tint = 0xffffff;},100);
     }, this);
@@ -120,9 +111,8 @@ function XtraUIbuttons(){
     plusD.inputEnabled = true;
     plusD.events.onInputDown.add(function(){
     	backAngle += 0.1;
-    	backText.text = "Back\nAngle: " + roundIt(INIT_BACK + backAngle);
+    	backText.text = "BACK: " + roundIt(INIT_BACK + backAngle);
     	plusD.tint = 0xf04030;
-    	//circle.scale.set(INIT_SIZE + distanceFactor);
     	setTimeout(function(){plusD.tint = 0xffffff;},100);
     }, this);
     
@@ -132,19 +122,14 @@ function XtraUIbuttons(){
     minusD.inputEnabled = true;
     minusD.events.onInputDown.add(function(){
     	backAngle -= 0.1;
-    	backText.text = "Back\nAngle: " + roundIt(INIT_BACK + backAngle);
+    	backText.text = "BACK: " + roundIt(INIT_BACK + backAngle);
     	minusD.tint = 0xf04030;
-    	//circle.scale.set(INIT_SIZE + distanceFactor);
     	setTimeout(function(){minusD.tint = 0xffffff;},100);
     }, this);
 	
-    backText = game.add.text(530, 30, "Back\nAngle: " + roundIt(INIT_BACK + backAngle),
-    {font: '22px', fill: 'white'});
+    backText = game.add.text(530, 30, "BACK: " + roundIt(INIT_BACK + backAngle),
+    {font: '23px', fill: 'white'});
     
-    frontText = game.add.text(530, 230, "Front\nAngle: " + roundIt(INIT_FRONT + frontAngle),
-    {font: '22px', fill: 'white'});
-}
-
-function onError(){
-	alert('error');
+    frontText = game.add.text(530, 230, "FRONT: " + roundIt(INIT_FRONT + frontAngle),
+    {font: '23px', fill: 'white'});
 }
