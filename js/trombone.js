f3e335ef37f5044fa961b41d00f5ebd72f7efc7b
johnnytal <johnnytal9@gmail.com>
2019-07-11 17:27:21 +0200

    improved tromb

--- js/trombone.js
+++ js/trombone.js
@@ -1,11 +1,13 @@
 var trombMain = function(game){
 	oleTriggered = false;
+
+	POINT = 15;
 };
 
 trombMain.prototype = {
     create: function(){ 
     	loadHorn();
-    	game.stage.backgroundColor = '#0f5420';
+    	game.stage.backgroundColor = '#0000ff';
 
     	oleTriggered = false;
 
@@ -17,20 +19,29 @@ function readTrombAccel(event){
 	if (game.state.getCurrentState().key == "Trombone"){
 		angleTromb = roundIt(event.beta);
 		
-		if (angleTromb < 0 && !sfxOle.isPlaying){
+		if (angleTromb < POINT && !sfxOle.isPlaying && !sfxOh.isPlaying){
 			sfxOh.play();
-			if (oleTriggered){
-				oleTriggered = false;
-			}
+			oleTriggered = false;
+			
+			game.stage.backgroundColor = '#0000ff';
 		}
-		else if (!oleTriggered && angleTromb >= 0){
+		else if (!oleTriggered && angleTromb >= POINT){
 			sfxOle.play();
 			oleTriggered = true;
+			
+			game.stage.backgroundColor = '#ffff00';
+			
+			window.plugins.flashlight.switchOn();
+			navigator.vibrate(500);	
 			
 			setTimeout(function(){
 				sfxOh.stop();
 			}, 100);
 		}
+	}
+	
+	if (!sfxOle.isPlaying && window.plugins.flashlight.isSwitchedOn()){
+		window.plugins.flashlight.switchOff();
 	}
 }
 
